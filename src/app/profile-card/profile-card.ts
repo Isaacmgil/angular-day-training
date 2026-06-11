b5281812c700e9efdc13b62ac2ea1c4bd0e8d763
Isaac Meneses <iantoniomgil@gmail.com>
2026-06-11 11:30:41 -0400

    feat: día 76 completado, [] bindings event bindings and {{}} conexión del ts y el html.

--- src/app/profile-card/profile-card.ts
+++ src/app/profile-card/profile-card.ts
@@ -6,4 +6,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './profile-card.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProfileCard {}
+export class ProfileCard {
+
+  nombreUsuario = 'Isaac Meneses';
+  avatarUrl = 'https://mangakaart.fandom.com/es/wiki/Kratos';
+  saludar(){
+    alert(`Hola, ${this.nombreUsuario}! Bienvenido a tu perfil.`);
+  }
+
+}
