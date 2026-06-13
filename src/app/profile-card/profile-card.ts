68f9b08ce8e571cf9675c6c255e7097bca4ea1ba
Isaac Meneses <iantoniomgil@gmail.com>
2026-06-13 18:43:50 -0400

    feat: día 78, practicando @if y @for

--- src/app/profile-card/profile-card.ts
+++ src/app/profile-card/profile-card.ts
@@ -16,4 +16,11 @@ export class ProfileCard {
   mostrarInfo = true;
   habilidades: string[] = ['Angular', 'TypeScript', 'Tailwind CSS'];
 
+  transacciones = [
+    { tipo: 'ingreso', descripcion: 'Trading XAU/USD', monto: 150.50, completado: true },
+    { tipo: 'gasto', descripcion: 'Suscripción Cinex', monto: 12.00, completado: true },
+    { tipo: 'gasto', descripcion: 'Servidores AWS', monto: 45.00, completado: false },
+    { tipo: 'ingreso', descripcion: 'Pago Freelance', monto: 300.00, completado: true }
+  ];
+
 }
