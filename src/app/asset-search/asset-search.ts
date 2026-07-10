5a65373984cbaa4387c74dedbb6697df340cc55e
Isaacmgil <iantoniomgil@gmail.com>
2026-07-10 12:04:57 -0400

    feat: día 89 completado, se logró hacer un filtrado con botones usando otra señal y combinandola en nuestro método de filtrado original.

--- src/app/asset-search/asset-search.ts
+++ src/app/asset-search/asset-search.ts
@@ -1,30 +1,35 @@
+import { TitleCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-asset-search',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './asset-search.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AssetSearch {
 
   activos = signal([
-  { id: 1, simbolo: 'BTC/USD' },
-  { id: 2, simbolo: 'ETH/USD' },
-  { id: 3, simbolo: 'XRP/USD' },
-  { id: 4, simbolo: 'LTC/USD' },
-  { id: 5, simbolo: 'BCH/USD' },
-  { id: 6, simbolo: 'ADA/USD' },
-]);
+    { id: 1, simbolo: 'BTC/USD', tipo: 'ingreso' },
+    { id: 2, simbolo: 'ETH/USD', tipo: 'ingreso' },
+    { id: 3, simbolo: 'XRP/USD', tipo: 'gasto' },
+    { id: 4, simbolo: 'LTC/USD', tipo: 'ingreso' },
+    { id: 5, simbolo: 'BCH/USD', tipo: 'gasto' },
+    { id: 6, simbolo: 'ADA/USD', tipo: 'gasto' },
+  ]);
 
   searchTerm = signal('');
+  tiposFiltro = ['todos', 'ingreso', 'gasto'] as const;
+  tipoSeleccionado = signal<'todos' | 'ingreso' | 'gasto'>('todos');
 
   activosFiltrados = computed(() => {
-  const termino = this.searchTerm().toLowerCase();
-  return this.activos().filter(activo =>
-    activo.simbolo.toLowerCase().includes(termino)
-  );
-});
+    const termino = this.searchTerm().toLowerCase();
+    return this.activos().filter(activo =>
+      (activo.simbolo.toLowerCase().includes(termino)) && (this.tipoSeleccionado() === 'todos' || activo.tipo === this.tipoSeleccionado())
+    );
+  });
+
+
 
 
 
