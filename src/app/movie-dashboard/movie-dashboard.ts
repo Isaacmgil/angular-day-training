944761ba42caf45beeb498693070c6aec96d9647
Isaac Meneses <iantoniomgil@gmail.com>
2026-07-26 18:55:03 -0400

    feat: dia 96 terminado, formularios reactivos! listos!

--- src/app/movie-dashboard/movie-dashboard.ts
+++ src/app/movie-dashboard/movie-dashboard.ts
@@ -1,15 +1,35 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-dashboard',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './movie-dashboard.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MovieDashboard {
 
   moviesservice = inject(MoviesService);
+
+  formularioMovie = new FormGroup({
+    titulo: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    genero: new FormControl('', [Validators.required, Validators.minLength(3)]),
+
+  })
+
+  guardarPelicula() {
+    // Podemos preguntar si todo el formulario está correcto
+    if (this.formularioMovie.valid) {
+      const peliculaData = this.formularioMovie.value;
+
+      this.moviesservice.agregarPelicula(peliculaData.titulo || '', peliculaData.genero || '')
+    }
+
+    // Borra todos los campos y reinicia las validaciones de golpe
+    this.formularioMovie.reset();
+
+  }
 
 
 }
